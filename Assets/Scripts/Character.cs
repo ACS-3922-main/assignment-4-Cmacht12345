519f7d39cf6d657f1e8d13d34159c2040e56682e
Cmacht12345 <coltonmacht2001@gmail.com>
2022-03-25 20:02:20 -0500

    Part A Complete
    Part A is Complete

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,15 +1,40 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+public enum CharacterCategory
+{
+    Player,
+    Enemy
+}
 
-public abstract class Character : MonoBehaviour {
-    [SerializeField] protected int _maxHitPoints = 10;
-    [SerializeField] protected float _hitPoints = 5;
+public abstract class Character : MonoBehaviour
+{
+    public CharacterCategory characterCategory;
+    [SerializeField] protected float _maxHitPoints = 10;
+    [SerializeField] protected float _startingHitPoints = 5;
+    public float MaxHitPoints
+    {
+        get
+        {
+            return _maxHitPoints;
+        }
+        set
+        {
+            _maxHitPoints = value;
+        }
+    }
 
-    public int MaxHitPoints {
-        get { 
-            return _maxHitPoints; 
-        } 
-        set { 
-            _maxHitPoints = value; 
-        } 
+    public virtual void KillCharacter()
+    {
+        Destroy(gameObject);
+    }
+
+    public abstract void ResetCharacter();
+    public abstract IEnumerator DamageCharacter(int damage, float interval);
+    public virtual IEnumerator FlickerCharacter()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        GetComponent<SpriteRenderer>().color = Color.white;
     }
 }
